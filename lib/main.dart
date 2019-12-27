656bb03ac509e24db7187bb21b71a18a1c5e7d72
Shubham Goswami <sgshubham98@gmail.com>
2019-12-27 19:22:50 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// starting of all flutter apps
+void main() => runApp(
+  MaterialApp(
+    home: Center(
+      child: Text("Hello Flutter"),
+    ),
+  ),
+);
