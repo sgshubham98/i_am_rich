9d690087efda0e9be03d88dfdc27b547e29f8c52
Shubham Goswami <sgshubham98@gmail.com>
2019-12-27 20:46:51 +0530

    lect 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 
 // starting of all flutter apps
 void main() => runApp(
-  MaterialApp(
-    home: Center(
-      child: Text("Hello Flutter"),
-    ),
-  ),
-);
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey[300],
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey[900],
+            title: Text('I Am Rich'),
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage('https://flutter.dev/assets/create/create-hero-illo-9da494ad02275eb43e8db8bacfea4bbc5e44efa21db57efc23c3d6ead3f36c14.png'),
+            ),
+          ),
+        ),
+      ),
+    );
