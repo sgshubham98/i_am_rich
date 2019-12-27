0f0634b91a4ab460a8f37a59dd412587c66ff538
Shubham Goswami <sgshubham98@gmail.com>
2019-12-27 21:08:21 +0530

    lect 3

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          backgroundColor: Colors.blueGrey[300],
           appBar: AppBar(
             backgroundColor: Colors.blueGrey[900],
             title: Text('I Am Rich'),
           ),
           body: Center(
             child: Image(
-              image: NetworkImage('https://flutter.dev/assets/create/create-hero-illo-9da494ad02275eb43e8db8bacfea4bbc5e44efa21db57efc23c3d6ead3f36c14.png'),
+              image: AssetImage('asset/images/diamond.png'),
             ),
           ),
         ),
